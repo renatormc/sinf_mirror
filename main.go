2c989ad4720fbebbf187887d2bb4e65f2e1a5eca
renatormc <renatomartinsrmc@gmail.com>
2020-04-24 09:01:03 -0300

    Adicionado argparse

--- main.go
+++ main.go
@@ -2,16 +2,29 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"time"
+
+	"github.com/akamensky/argparse"
 )
 
 func main() {
+
+	parser := argparse.NewParser("sinf-mirror", "Mirror one folder to another")
+	source := parser.String("s", "source", &argparse.Options{Required: true, Help: "Folder to be mirrored"})
+	dest := parser.String("d", "destination", &argparse.Options{Required: true, Help: "Folder to mirror to"})
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Print(parser.Usage(err))
+		return
+	}
+
 	synchronizer := new(Synchronizer)
 	synchronizer.totalSize = 0
 	synchronizer.totalNumber = 0
 	synchronizer.currentSize = 0
-	synchronizer.source = "D:\\teste_report\\C3"
-	synchronizer.dest = "D:\\teste_report\\c1_copia_deletar"
+	synchronizer.source = *source
+	synchronizer.dest = *dest
 	synchronizer.verbose = true
 	synchronizer.startTime = time.Now()
 	synchronizer.appConfig = GetConfig()
