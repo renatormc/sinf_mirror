f785cbac861c6c2ec1a85569521c2f1a6c1a1acc
renatormc <renatomartinsrmc@gmail.com>
2020-09-01 15:30:14 -0300

    Normla

--- run.bat
+++ run.bat
@@ -3,4 +3,4 @@ REM go build -a -o "%SINFTOOLS%\tools\sinf_mirror\sinf_mirror.exe" && s-mirror -
 REM go build -a -o "%SINFTOOLS%\tools\sinf_mirror\sinf_mirror.exe" && s-mirror -s "E:\Dados da Prefeitura de Silv�nia" -d "I:\Dados da Prefeitura de Silv�nia"
 REM go build -a -o "%SINFTOOLS%\tools\sinf_mirror\sinf_mirror.exe" && s-mirror -f inputs.txt
 go build -a -o "%SINFTOOLS%\tools\sinf_mirror\sinf_mirror.exe" && s-mirror -c case_test
-REM s-mirror -f inputs.txt
+REM s-mirror -f inputs.txt asdf
